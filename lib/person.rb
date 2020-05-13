9abd7e55f688e1b4b14194c6fdc3a18714f7123b
PARDEEP KAUR <pkaur84@aol.com>
2020-05-13 06:29:44 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,15 @@
 class Person
-  def initialize (persons_name)
-    @name = persons_name
+
+  def initialize(name)
+    @name = name
   end
 
-  def name=(persons_name)
-    @name = persons_name
+  def name=(name)
+    @name = name
   end
 
   def name
     @name
   end
-  
+
 end
-  
