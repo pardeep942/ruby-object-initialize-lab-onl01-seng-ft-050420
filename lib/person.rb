91336ba855c625231b80df806d24d60a92a40a1c
PARDEEP KAUR <pkaur84@aol.com>
2020-05-13 06:28:33 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,15 @@
-lass personc 
+class Person
+  def initialize (persons_name)
+    @name = persons_name
+  end
+
+  def name=(persons_name)
+    @name = persons_name
+  end
+
+  def name
+    @name
+  end
+  
+end
   
